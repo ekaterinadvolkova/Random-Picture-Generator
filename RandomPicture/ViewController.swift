8ed1e532f5c04e9e7a454ad7c9400ec03ca7a467
Kateridzhe <ekaterina.d.volkova@gmail.com>
2021-11-27 23:03:30 +0300

    Initial Commit

---
+++ RandomPicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPicture
+//
+//  Created by Ekaterina Volkova on 27/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
